d77415d371b0083bfd0d5f557ddbad26d0f4e40a
Harsh Sharma <hs804506@gmail.com>
2024-02-22 22:55:33 +0530

    updated code for toggle functionality

--- ToggleButton/blog/src/App.js
+++ ToggleButton/blog/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [show, setShow] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = { () => setShow(!show) }>Toggle Button</button>
+      <button onClick={ () => setShow(true) }>Show Button</button>
+      <button onClick={ () => setShow(false) }>Hide Button</button>
+      { show ? <h1>Hello World!</h1> : <></>}
     </div>
   );
 }
