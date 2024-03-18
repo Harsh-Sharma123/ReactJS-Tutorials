557d124768320c9f42bbd5f913d3e6422a9a7778
Harsh Sharma <hs804506@gmail.com>
2024-03-18 21:18:33 +0530

    added table data

--- ArraysTableWithBootstrap/blog/src/App.js
+++ ArraysTableWithBootstrap/blog/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const people = [
+    { name: 'a', age: 20, email: 'a@gmail.com' },
+    { name: 'b', age: 22, email: 'b@gmail.com' },
+    { name: 'c', age: 23, email: 'c@gmail.com' },
+    { name: 'd', age: 24, email: 'd@gmail.com' }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table border="2">
+        <thead border="2">
+          <th>Name</th>
+          <th>Age</th>
+          <th>Email</th>
+        </thead>
+        <tbody border="1">
+          {people.map((person) => (
+            <tr border="2">
+              <td>{person.name}</td>
+              <td>{person.age}</td>
+              <td>{person.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
