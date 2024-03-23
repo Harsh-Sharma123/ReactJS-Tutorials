994004656b4146707aa03172f08acd3700f7dc30
Harsh Sharma <hs804506@gmail.com>
2024-03-23 12:46:02 +0530

    added pure component code

--- PureComponentInReact/blog/src/App.js
+++ PureComponentInReact/blog/src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.PureComponent{
+  constructor(){
+    super();
+    this.state = {
+      count: 1
+    }
+  }
+  render(){
+    console.log("Re-Render Check!");
+    return (
+      <>
+        <div className='App'>
+          <h2>Pure Component Demo in React</h2>
+          <h4>Count : {this.state.count}</h4>
+          <button onClick={()=>this.setState({count: this.state.count})}>Click Me</button>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
