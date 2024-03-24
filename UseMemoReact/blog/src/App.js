71d9cae0810b12d15e051eb31b6d832bd8936864
Harsh Sharma <hs804506@gmail.com>
2024-03-24 16:20:51 +0530

    use memo demo in reactjs

--- UseMemoReact/blog/src/App.js
+++ UseMemoReact/blog/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useMemo } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [item, Itemi] = useState(10);
+
+  // function countCheck(){
+  //   console.log("Count Check Console!");
+  //   return count*2;
+  // }
+
+  const checkCountMemo = useMemo(()=>{
+    console.log("Count From UseMemo");
+    return count*2;
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Use Memo Demo in ReactJS</h2>
+      <h4>Count : {count}</h4>
+      <h4>Item : {item}</h4>
+      {/* <h4>Count from Function : {countCheck()}</h4> */}
+      <h4>Count from Function UseMemo : {checkCountMemo}</h4>
+      <button onClick={()=>setCount(count + 1)}>Update Count</button>
+      <button onClick={()=>Itemi(item*10)}>Update Item</button>
     </div>
   );
 }
