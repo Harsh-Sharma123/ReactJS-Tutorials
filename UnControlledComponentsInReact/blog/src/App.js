cfba941fb6305d22b8cba8e3c218fa1f95fb3c3a
Harsh Sharma <hs804506@gmail.com>
2024-04-03 22:29:06 +0530

    updated uncontrolled compopnent code

--- UnControlledComponentsInReact/blog/src/App.js
+++ UnControlledComponentsInReact/blog/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useRef} from 'react';
 
 function App() {
+
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(inputRef1.current.value);
+    console.log(inputRef2.current.value);
+    const input3 = document.getElementById("3");
+    console.log(input3.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Uncontrolled Components!</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef1} />
+        <input type="text" ref={inputRef2} />
+        <input type="text" id="3" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
