ab449c386687808b0ce3495a22634108c0681b45
Harsh Sharma <hs804506@gmail.com>
2024-04-05 21:37:39 +0530

    higher order component example

--- HigherOrderComponentInReact/blog/src/App.js
+++ HigherOrderComponentInReact/blog/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import HOCGreen from './HOC';
+import HOCRed from './HOCRed';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Higher Order Component In ReactJS</h2>
+      <HOCGreen comp={Counter} />
+      <HOCRed comp={Counter} />
     </div>
   );
 }
