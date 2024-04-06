47fbcfdb61528e34a46a82c11a3204d39c560bed
Harsh Sharma <hs804506@gmail.com>
2024-04-06 15:57:48 +0530

    react-router-example

---
+++ ReactRouterDomInReact/blog/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Contact from './Contact';
+import Home from './Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
