3d885ef658c07ef3ead12c467105504e854d2634
Harsh Sharma <hs804506@gmail.com>
2024-04-16 22:12:17 +0530

    fetch-api-fixed

--- FetchAPIGetMethod/blog/src/App.js
+++ FetchAPIGetMethod/blog/src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/').then((result) => {
+      result.json().then((res) => {
+        console.log(res);
+        setData(res);
+        console.log(data)
+      })
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>
+              ID
+            </th>
+            <th>
+              Title
+            </th>
+            <th>
+              Completed
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {
+            data.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.completed ? 'True' : 'False'}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
     </div>
   );
 }
