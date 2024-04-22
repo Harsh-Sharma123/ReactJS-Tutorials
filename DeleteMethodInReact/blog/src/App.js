4b0ee1611cd3ef27722e50ac0fc7360b0a30d2ea
Harsh Sharma <hs804506@gmail.com>
2024-04-22 22:59:35 +0530

    delete request in react

--- DeleteMethodInReact/blog/src/App.js
+++ DeleteMethodInReact/blog/src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/').then((result)=>{
+      result.json().then((res)=>{
+        console.log(res);
+        setData(res)
+      })
+    })
+  })
+
+  function deleteUser(id){
+    fetch(`https://localhost:8000/${id}`, {
+      method: 'delete'
+    }).then((result)=>{
+      result.json().then((res)=>{
+        console.log(res);
+        fetch('https://jsonplaceholder.typicode.com/todos/').then((result)=>{
+          result.json().then((res)=>{
+            console.log(res);
+            setData(res)
+          })
+        })
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Completed</th>
+          </tr>
+        </thead>
+        <tbody>
+     {
+       data.map((item)=>(
+         <tr>
+            <td>
+              {item.id}
+            </td>
+            <td>{item.title}</td>
+            <td>{item.completed ? 'True' : 'False'}</td>
+         </tr>
+         ))
+        }
+        </tbody>
+        </Table>
     </div>
   );
 }
