331847e4f553768b2bb1d0e268b9e20d41fdf3b8
Harsh Sharma <hs804506@gmail.com>
2024-04-27 15:50:52 +0530

    added example of put method request

--- UpdateDataWithApiInReact/blog/src/App.js
+++ UpdateDataWithApiInReact/blog/src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Table } from 'react-bootstrap';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const [id, setId] = useState('');
+  const [title, setTitle] = useState('');
+  const [isCompleted, setIsCompleted] = useState('');
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/').then((result)=>{
+      result.json().then((res)=>{
+        setData(res.slice(0, 10));
+      })
+    })
+  }, []);
+
+  const handleClick = (id) => {
+    console.log(id);
+    setId(id);
+    setTitle(data[id-1].title);
+    setIsCompleted(data[id-1].isCompleted);
+  };
+
+
+  function handleUpdate(){
+    let item = {id, title, isCompleted};
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'applicatoin/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    }).then((result)=>{
+      result.json().then((res)=>{
+        setData(res.slice(0, 10));
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped hover bordered>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Completed</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item)=>(
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.isCompleted ? 'True' : 'False'}</td>
+                <td><button onClick={()=>handleClick(item.id)}>Update Field</button></td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+
+      <form >
+        <label>Id : <input type="text" value={id} ></input></label>
+        <label>Title : <input type="text" value={title} ></input></label>
+        <label>isCompleted : <input type="text" value={isCompleted ? 'True' : 'False'} ></input></label>
+        <button onClick={handleUpdate}>Update</button>
+      </form>
     </div>
   );
 }
